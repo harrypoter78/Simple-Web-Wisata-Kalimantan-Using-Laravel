fcbcd4795d84d3911e605e3d699325c5ca45720a
dwkirza <67088311+dwkirza@users.noreply.github.com>
2022-03-21 21:13:25 +0700

    4

---
+++ resources/views/category.blade.php
@@ -0,0 +1,17 @@
+
+<link rel="stylesheet" href="../css/app.css">
+@extends('layout.main')
+@section('container')
+
+<h1>Post Category : {{ $category }}</h1>
+
+    @foreach ($posts as $post)
+    <article>
+    <h2> <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+    <p>{{ $post->excerpt}}</p>
+</article>
+        
+    @endforeach
+@endsection
+
+ 
