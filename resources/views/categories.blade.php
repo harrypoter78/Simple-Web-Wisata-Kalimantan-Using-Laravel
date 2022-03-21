864ce0ce3b5ee6f4c8cbac9fbfa3d077e6e8bc6b
dwkirza <67088311+dwkirza@users.noreply.github.com>
2022-03-22 00:13:09 +0700

    tampilan kategori

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -36,53 +36,20 @@
   <div class="album py-5 bg-light">
     <div class="container"  >
 
-      <div class="row row-cols-1 row-cols-sm-2 g-4">
+      <div class="row row-cols-1 row-cols-sm-2 g-4" style="color: aqua">
         @foreach ($categories as $category)
         <div class="col">
           <div class="card shadow-sm d-flex justify-content-center">
             {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" src="https://source.unsplash.com/1200x500?nature,city" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> --}}
-            <img  src="https://source.unsplash.com/1200x500?nature,city" style="height: 430; color-img"  >
-            <div class="card-img-overlay position-absolute text-exception"  >
-              <p class="card-text "><a class="text-decoration-none text-white" href="/categories/{{ $category->slug }}" class="text-decoration-none "> {{ $category->name }}</a></p>
-              <p class="card-text text-white ">KALIMANTAN</p>
+            <img  src="https://source.unsplash.com/1200x500?mountain,city" style="height: 430; color-img"  >
+            <div class="card-img-overlay d-flex align-items-center" style="background-color: rgba(0,0,0,0.5) ">
+              <p class="card-title fs-1 text-center flex-fill "><a class="text-decoration-none  text-white" href="/categories/{{ $category->slug }}" class=" "> {{ $category->name }}</a></p>
+              {{-- <p class="card-text text-white mt-0">KALIMANTAN</p> --}}
             </div>
           </div>
         </div>
         @endforeach
 
-        {{-- <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div> --}}
       </div>
     </div>
   </div>
